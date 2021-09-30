95dec2f30c46a741a9307dcfcddea4a8ecde6213
Abraham Gonzalez <abe.gonzalez@berkeley.edu>
2021-09-30 23:50:58 +0000

    Fix post run script | Update gitignore

--- host-analysis.sh
+++ host-analysis.sh
@@ -6,6 +6,7 @@ set -ex
 # $1 - <PATH_TO_OUTPUT>
 
 WORKLOAD_DIR=$PWD
-OUT_DIR=$1
+WORKLOAD_NAME=$1
+OUT_DIR=$2
 
-$WORKLOAD_DIR/analysis/parse-ps-log-overall.py $OUT_DIR/prof-stethoscope.log
+$WORKLOAD_DIR/analysis/parse-ps-log-overall.py $OUT_DIR/$WORKLOAD_NAME/prof-stethoscope.log
