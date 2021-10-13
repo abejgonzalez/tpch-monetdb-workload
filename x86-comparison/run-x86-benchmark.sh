b848adac6ce5420e766147d62fdd1a95caecb287
Abraham Gonzalez <abe.gonzalez@berkeley.edu>
2021-10-13 20:49:08 +0000

    Move logs into log dir

--- x86-comparison/run-x86-benchmark.sh
+++ x86-comparison/run-x86-benchmark.sh
@@ -16,12 +16,9 @@ cd $THIS_DIR
 echo "Launch instance"
 pushd $AWS_TOOLS_DIR
 # verify instance is off
-python awstools.py terminate --inst_type m5.large --clustertag benchtestx86 | true
+python awstools.py terminate --inst_type m5.large --clustertag benchtestx86 2>/dev/null | true
 python awstools.py launch --inst_type m5.large --inst_amt 1 --clustertag benchtestx86 2>&1 | tee $IP_ADDR_FILE
 popd
-
-echo "Wait for instance to boot"
-sleep 5m
 
 IP_ADDR=$(grep -E -o "192\.168\.[0-9]{1,3}\.[0-9]{1,3}" $IP_ADDR_FILE | head -n 1)
 
@@ -55,7 +52,16 @@ run_script ./host-run-experiment.sh $SF $THREADS
 copy $IP_ADDR:tpch-scripts/03_run/SF1-noprof-results/ SF1-noprof-results
 copy $IP_ADDR:tpch-scripts/03_run/SF1-prof-results/ SF1-prof-results
 
+LOG_DIR=$THIS_DIR/logs/$(date +"%d-%b-%Y-%H-%M-%S")
+mkdir -p $LOG_DIR
+mv SF1-noprof-results $LOG_DIR/
+mv SF1-prof-results $LOG_DIR/
+
 echo "Shutting down instance"
 pushd $AWS_TOOLS_DIR
 python awstools.py terminate --inst_type m5.large --clustertag benchtestx86 2>&1 | tee $IP_ADDR_FILE
 popd
+
+rm -rf $IP_ADDR_FILE
+
+echo "Successfully ran benchmark"
